e2db8c3129a51885761debe12c774b8ace34671f
peter-loliveira <peter.loliveira@gmail.com>
2020-02-28 16:33:11 -0300

    Inicio JavaScript

---
+++ js/js.js
@@ -0,0 +1,18 @@
+var frutas = [{nome: "Maça", cor: "Vermelha"}, {nome:"Uva", cor: "Roxa"}]
+console.log(frutas)
+console.log(frutas[0].nome + " " + frutas[0].cor)
+
+// var x = "Teste de TROCA:", y = "Teste mal sucessido!"
+// console.log(x + " " + y)
+// console.log(x + " " + y.replace("mal", "BEM"))
+
+// var listaFrutas = ["Maça", "Pera", "Banana"]
+// console.log(listaFrutas)
+// console.log(listaFrutas[1])
+// listaFrutas.push("Uva")//Adiciona um item a lista
+// console.log(listaFrutas)
+// listaFrutas.pop() // retira o ultimo item da listaFrutas. Se colocado indice retira o elemendo daquele indice
+// console.log(listaFrutas)
+// console.log(listaFrutas.reverse()) //Inverte a ordem de exibição dos elementos
+// console.log(listaFrutas.toString()) //exibe os itens da lista em forto de string
+
