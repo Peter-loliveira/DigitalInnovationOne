8fc4327274b05cedc4092e27dc8c51690220d200
peter-loliveira <peter.loliveira@gmail.com>
2020-03-04 11:59:37 -0300

    Spread

--- js/spread.js
+++ js/spread.js
@@ -1,6 +1,20 @@
 var x = "Peter"
 var y = " Lange Oliveira"
 
+var Peter = []
+for (i = 0; i < x.length; i++){
+    Peter.push(x[i])
+}
+for (i = 0; i < y.length; i++){
+    Peter.push(y[i])
+}
+console.log(Peter)
+
+peter = []//Zera o array para o exemplo abaixo
+//Tudo, desde a linha 5 até a linha 10 pode ser escrito como na linha a seguir:
+peter = [...x, ...y]
+console.log(Peter)
+
 var xy = x + y
 console.log(xy)
 
@@ -16,3 +30,13 @@ console.log(xy.length)
 var xy = [...x, ...y, ..." Utilizando Spread"]
 console.log(xy)
 console.log(xy.length)
+
+var mussum = []
+mussum = [..."Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor."]
+console.log(mussum)
+console.log("Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.")
+
+var teste = []
+var teste2 = [1,2,3,6,5,4,8,9]
+teste = [...teste2]
+console.log(teste)
