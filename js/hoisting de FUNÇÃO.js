7401b0a83a30fdd80dfac035c4e70444b125bad7
peter-loliveira <peter.loliveira@gmail.com>
2020-03-04 11:33:55 -0300

    Operador Spread

---
+++ js/hoisting de FUNÇÃO.js
@@ -0,0 +1,15 @@
+// const user = {
+//     name: "Peter",
+//     lastName: "Lange Oliveira"
+// }
+
+// function getUserWithFullName(user) {
+//     return {
+//         ...user,
+//         fullName: `${user.name} ${user.lastName}`
+//     }
+// }
+
+// const userWhithFullName = getUserWithFullName(user)
+
+// console.log(userWhithFullName)
---
+++ js/spread.js
@@ -0,0 +1,18 @@
+var x = "Peter"
+var y = " Lange Oliveira"
+
+var xy = x + y
+console.log(xy)
+
+var xy = [x, y]
+console.log(xy)
+
+var xy = []
+console.log(xy)
+
+console.log(...x, ...y, ..." Utilizando Spread")
+console.log(xy.length)
+
+var xy = [...x, ...y, ..." Utilizando Spread"]
+console.log(xy)
+console.log(xy.length)
